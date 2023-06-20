b3ccbbf3bdfc589906a2d4e463d74284e08c7632
paulinagacek <paulina.gacek.pl@gmail.com>
2023-06-20 21:20:27 +0200

    final

--- run.py
+++ run.py
@@ -10,32 +10,38 @@ MAX_ITER=1000 # max nr of iterations
 
 directory = os.getcwd()
 
-df = pd.read_csv(os.path.join('/opt/models/', INPUT_NAME))
-ROWS=len(df.axes[0]) + 1 
-COLS=len(df.axes[1]) 
-MAX_ROW_NR = ROWS-1
-MAX_COL_NR = COLS-1
+def create_experiment_netlogo():
+    df = pd.read_csv(os.path.join(MODEL_FOLDER, INPUT_NAME))
+    ROWS=len(df.axes[0]) + 1 
+    COLS=len(df.axes[1]) 
+    MAX_ROW_NR = ROWS-1
+    MAX_COL_NR = COLS-1
 
-with open(EXPERIMENT_LAYOUT_FILE, 'r') as file:
-    data = file.read()
-    data = data.replace('MAX_ITER', str(MAX_ITER))
-    data = data.replace('ROWS', str(ROWS))
-    data = data.replace('COLS', str(COLS))
-    data = data.replace('MAX_ROW_NR', str(MAX_ROW_NR))
-    data = data.replace('MAX_COL_NR', str(MAX_COL_NR))
+    with open(EXPERIMENT_LAYOUT_FILE, 'r') as file:
+        data = file.read()
+        data = data.replace('MAX_ITER', str(MAX_ITER))
+        data = data.replace('ROWS', str(ROWS))
+        data = data.replace('COLS', str(COLS))
+        data = data.replace('MAX_ROW_NR', str(MAX_ROW_NR))
+        data = data.replace('MAX_COL_NR', str(MAX_COL_NR))
+        data = data.replace('INPUT', str(INPUT_NAME))
 
-    with open(os.path.join(MODEL_FOLDER, EXPERIMENT_FILE), 'w') as exp_file:
-        exp_file.truncate(0)
-        exp_file.write(data)
+        with open(os.path.join(MODEL_FOLDER, EXPERIMENT_FILE), 'w') as exp_file:
+            exp_file.truncate(0)
+            exp_file.write(data)
 
-REPETITIONS = 5
-start = time.time()
-for idx in range(REPETITIONS):
-    subprocess.run(["./NetLogo 6.3.0/NetLogo_Console","--headless", 
-        "--model", "/opt/models/NLModel.nlogo" ,
-        "--setup-file", "/opt/models/experiment.xml" ,
-        "--table", "models/table-output.csv ",
-        "--spreadsheet", "models/table-output.csv"],  stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+input_names = ['10x10board.csv', '100x100board.csv', '1000x1000board.csv']
+for INPUT_NAME in input_names:
+    create_experiment_netlogo()
 
-end = time.time()
-print((end - start)/5)
+    REPETITIONS = 1
+    start = time.time()
+    for idx in range(REPETITIONS):
+        subprocess.run(["./NetLogo 6.3.0/NetLogo_Console","--headless", 
+            "--model", "/opt/models/NLModel.nlogo" ,
+            "--setup-file", "/opt/models/experiment.xml" ,
+            "--table", "models/table-output.csv ",
+            "--spreadsheet", "models/table-output.csv"],  stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+    end = time.time()
+    print((end - start)/REPETITIONS)
