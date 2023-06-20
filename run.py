ab9387d6e89416250f6dbfb8452a78f92dd2af98
paulinagacek <paulina.gacek.pl@gmail.com>
2023-06-20 22:32:27 +0200

    paths corrected

--- run.py
+++ run.py
@@ -1,18 +1,18 @@
 import time, subprocess
 import pandas as pd, os
 
-MODEL_FOLDER='/opt/models/GameOfLife'
-INPUT_NAME='input.csv'
+MODEL_FOLDER='/opt/models'
+MODEL_NAME = 'GameOfLife'
 EXPERIMENT_LAYOUT_FILE='exp_layout.xml'
 EXPERIMENT_FILE='experiment.xml'
 
-MAX_ITER=1000 # max nr of iterations
-REPETITIONS = 1
+MAX_ITER=100 # max nr of iterations
+REPETITIONS = 5
 
 directory = os.getcwd()
 
-def create_experiment_netlogo():
-    df = pd.read_csv(os.path.join(MODEL_FOLDER, INPUT_NAME))
+def create_experiment_netlogo(INPUT_NAME):
+    df = pd.read_csv(os.path.join(MODEL_FOLDER, MODEL_NAME, INPUT_NAME))
     ROWS=len(df.axes[0]) + 1 
     COLS=len(df.axes[1]) 
     MAX_ROW_NR = ROWS-1
@@ -25,24 +25,26 @@ def create_experiment_netlogo():
         data = data.replace('COLS', str(COLS))
         data = data.replace('MAX_ROW_NR', str(MAX_ROW_NR))
         data = data.replace('MAX_COL_NR', str(MAX_COL_NR))
-        data = data.replace('INPUT', str(INPUT_NAME))
+        data = data.replace('INPUT', str(os.path.join(MODEL_NAME, INPUT_NAME)))
         data = data.replace('REPETITIONS', str(REPETITIONS))
 
         with open(os.path.join(MODEL_FOLDER, EXPERIMENT_FILE), 'w') as exp_file:
             exp_file.truncate(0)
             exp_file.write(data)
 
-input_names = ['10x10board.csv', '100x100board.csv', '1000x1000board.csv']
-for INPUT_NAME in input_names:
-    create_experiment_netlogo()
+# input_names = ['10x10board.csv', '100x100board.csv', '1000x1000board.csv']
+input_names = ['10x10board.csv', '100x100board.csv']
+# input_names = ['1000x1000board.csv']
+for input in input_names:
+    create_experiment_netlogo(input)
 
     start = time.time()
     subprocess.run(["./NetLogo 6.3.0/NetLogo_Console","--headless", 
         "--model", "/opt/models/NLModel.nlogo" ,
         "--setup-file", os.path.join(MODEL_FOLDER, EXPERIMENT_FILE) ,
         "--table", "models/table-output.csv ",
-        "--spreadsheet", "models/table-output.csv"],  
-        stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL
+        "--spreadsheet", "models/table-output.csv"] 
+        # stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL
         )
 
     end = time.time()
